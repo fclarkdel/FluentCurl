6bf3a6d6ede00ff2698b9fdc08cbe4f2db389893
fclarkdel <fernandoclarkdel@gmail.com>
2023-09-11 20:58:45 -0500

    Test server for testing

--- source/fluent_curl/session.hpp
+++ source/fluent_curl/session.hpp
@@ -1,14 +1,14 @@
 #ifndef FLUENTCURL_SESSION_HPP
 #define FLUENTCURL_SESSION_HPP
 
-#include <queue>
 #include <mutex>
+#include <queue>
 #include <thread>
 
 #include <curl/curl.h>
 #include <uv.h>
 
-#include <fluent_curl/curl_resource.hpp>
+#include <fluent_curl/curl/curl_resource.hpp>
 #include <fluent_curl/handle.hpp>
 
 namespace fluent_curl
@@ -31,73 +31,9 @@ public:
 	operator=(session&& move) = delete;
 
 	void
-	add_handle(const handle& handle);
+	perform(const handle& handle);
 
 private:
-	CURLM* _multi_handle;
-	uv_loop_t* _event_loop;
-	uv_timer_t* _timer;
-
-	bool _keep_thread_alive;
-	std::thread _thread;
-
-	std::mutex _queue_lock;
-	std::queue<CURL*> _queue;
-
-	void
-	process_handles();
-
-	static void
-	throw_on_curl_multi_error(CURLMcode result);
-
-	static int
-	throw_on_uv_error(int result);
-
-	// libuv structs and callbacks.
-	struct PollContext
-	{
-		CURLM* multi_handle;
-		curl_socket_t socket;
-	};
-
-	static void
-	poll_event_cb(
-		uv_poll_t* handle,
-		int status,
-		int events);
-
-	static void
-	poll_close_cb(uv_handle_t* handle);
-
-	static void
-	timeout_cb(uv_timer_t* handle);
-
-	static void
-	timer_close_cb(uv_handle_t* handle);
-
-	// curl structs and callbacks.
-	struct CurlContext
-	{
-		CURLM* multi_handle;
-		uv_loop_t* event_loop;
-	};
-
-	static int
-	socket_cb(
-		CURL* handle,
-		curl_socket_t socket,
-		int action,
-		void* shared_data,
-		void* socket_data);
-
-	static int
-	timer_cb(
-		CURLM* handle,
-		long timeout_ms,
-		void* timer_data);
-
-	static void
-	check_multi_info(CURLM* handle);
 };
 }
 #endif
